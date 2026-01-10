d1ab882930039a858d13a3da4606a0d332a74d09
Sayani <sayanirautdessai@gmail.com>
2026-01-10 13:46:03 +0530

    Changes commit

--- app/src/main/java/com/example/aspirepath/First.kt
+++ app/src/main/java/com/example/aspirepath/First.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -34,6 +35,19 @@ class First : AppCompatActivity() {
             }
             true
         }
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val currentFragment = supportFragmentManager.findFragmentById(R.id.frameLayout)
+                if (currentFragment is Profile) {
+                    replaceFragment(Home())
+                    bottomNavigationView.selectedItemId = R.id.home
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
